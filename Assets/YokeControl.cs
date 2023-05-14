5a23d5b1247b2d8123301bd0caa31d68ac7bd70c
Ailerios <51855522+Ailerios@users.noreply.github.com>
2023-05-15 01:13:08 +0200

    Intermediate! Add snap points on yoke; Add additional parent to fix non-uniform scaled parent being not legit; Start writing YokeControl.cs

---
+++ Assets/YokeControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YokeControl : MonoBehaviour
+{
+    // Right Hand
+    public GameObject rightHand;
+    private Transform rightHandOriginalParent;
+    private bool rightHandOnYoke = false;
+    public Transform rightSnap;
+
+    // Left Hand
+    public GameObject leftHand;
+    private Transform leftHandOriginalParent;
+    private bool leftHandOnYoke = false;
+    public Transform leftSnap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
