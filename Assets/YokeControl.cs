5d4f6b43593e85946d35e69114916049beacac0d
Ailerios <51855522+Ailerios@users.noreply.github.com>
2023-05-15 05:35:59 +0200

    TODO: Test and debug; Proto-finished Yoke Control; Update XR Interaction Tools; Setup Unity like in the tutorial EXCEPT universal scripted render pipeline; Add cubes as prototype hands

--- Assets/YokeControl.cs
+++ Assets/YokeControl.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class YokeControl : MonoBehaviour
 {
+    // XR Input Devices
+    
+
     // Right Hand
     public GameObject rightHand;
     private Transform rightHandOriginalParent;
@@ -16,6 +21,8 @@ public class YokeControl : MonoBehaviour
     private bool leftHandOnYoke = false;
     public Transform leftSnap;
 
+    public Transform directionalObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +33,77 @@ public class YokeControl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("PlayerHand"))
+        {
+            // Place Right Hand
+            InputDevice rightHandDev = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+            bool triggerValue;
+            if (rightHandOnYoke && rightHandDev.TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && triggerValue)
+            {
+                PlaceHandOnWheel(ref rightHand, ref rightSnap, ref rightHandOriginalParent, ref rightHandOnYoke);
+            }
+
+            // Place Left Hand
+            InputDevice leftHandDev = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            if (leftHandOnYoke && leftHandDev.TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && triggerValue)
+            {
+                PlaceHandOnWheel(ref leftHand, ref leftSnap, ref leftHandOriginalParent, ref leftHandOnYoke);
+            }
+        }
+    }
+
+    private void ReleaseHandsFromWheel() {
+        InputDevice rightHandDev = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        bool triggerValue;
+        if (rightHandOnYoke && rightHandDev.TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && !triggerValue)
+        {
+            rightHand.transform.parent = rightHandOriginalParent;
+            rightHand.transform.position = rightHandOriginalParent.position;
+            rightHandOnYoke = false;
+        }
+        InputDevice leftHandDev = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        if (leftHandOnYoke && leftHandDev.TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && !triggerValue)
+        {
+            leftHand.transform.parent = leftHandOriginalParent;
+            leftHand.transform.position = leftHandOriginalParent.position;
+            leftHandOnYoke = false;
+        }
+    }
+
+    private void PlaceHandOnWheel(ref GameObject hand, ref Transform snap, ref Transform originalParent, ref bool handOnYoke) {
+        originalParent = hand.transform.parent;
+        hand.transform.parent = snap.transform;
+        hand.transform.position = snap.transform.position;
+
+        handOnYoke = true;
+    }
+
+    private void ConvertHandRotationToYokeMovement() {
+        if (rightHandOnYoke && !leftHandOnYoke)
+        {
+            Quaternion newRot = Quaternion.Euler(0, 0, rightHandOriginalParent.transform.rotation.eulerAngles.z);
+            directionalObject.rotation = newRot;
+            transform.parent = directionalObject;
+        }
+        else if (!rightHandOnYoke && leftHandOnYoke)
+        {
+            Quaternion newRot = Quaternion.Euler(0, 0, leftHandOriginalParent.transform.rotation.eulerAngles.z);
+            directionalObject.rotation = newRot;
+            transform.parent = directionalObject;
+        }
+        else if (rightHandOnYoke && leftHandOnYoke)
+        {
+            Quaternion newRotLeft = Quaternion.Euler(0, 0, leftHandOriginalParent.transform.rotation.eulerAngles.z);
+            Quaternion newRotRight = Quaternion.Euler(0, 0, rightHandOriginalParent.transform.rotation.eulerAngles.z);
+            Quaternion finalRot = Quaternion.Slerp(newRotLeft, newRotRight, 1.0f / 2.0f);
+            directionalObject.rotation = finalRot;
+            transform.parent = directionalObject;
+        }
+
+
     }
 }
